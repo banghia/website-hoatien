67ebd5f3568b6ba2425f89e6bc1f536fdee899d9
banghia <banghia.violet@gmail.com>
2018-04-24 23:45:54 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+ 
+    </head>
+    <body>
+        <script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
+        <textarea name="content" class="form-control my-editor"></textarea>
+        <script src="{{ asset('js/script.js') }}"></script>
+    </body>
+</html>
